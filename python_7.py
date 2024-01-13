8ca38220caeb909c4fa7b8a4bb5f040f0c8807ef
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-01-13 18:39:06 +0530

    Completed till typecasting

---
+++ python_7.py
@@ -0,0 +1,24 @@
+'''
+oprators : 1. "+" --> Addition
+           2. "-" --> Substraction
+           3. "*" --> Multiplication
+           4. "/" --> Division
+           5. "//" --> Floor Division
+           6. "%" --> Modulo
+           7. "**" --> Exponential
+'''
+
+# Excercise No.1
+# Question --> Create a Calculator which is capabale for performing all mathematical oprations of all oprators
+
+# Answer -->
+
+a = 10
+b = 20
+print("Addition of ",a," and ",b," is ",a+b)
+print("Substration of ",a," and ",b," is ",a-b)
+print("Multiplication of ",a," and ",b," is ",a*b)
+print("Division of ",a," and ",b," is ",a/b)
+print("Floor Division of ",a," and ",b," is ",a//b)
+print("Module of ",a," and ",b," is ",a%b)
+print("Exponential of ",a," and ",b," is ",a**b)
---
+++ python_9.py
@@ -0,0 +1,18 @@
+'''
+Type Casting --> 1.The conversion of one DataType into another DataType is known as type casting or type conversion
+                 2.There are teo types of type casting :
+                        i.Explicite TypeCasting : The conversion of a data type into another data type by a programmer is known as explicite typecasting
+                        ii.Implicite TypeCasting : According to a level one data type is converted to another is known as implicite typecasting. Python converts a smaller datatype into a higher datattype to prevent data loss.
+'''
+# Example -->
+a = "23"
+b = int(a)
+print(type(b))
+
+c = 12
+d = str(c)
+print(type(d))
+
+x = 3.55
+y = 7
+print(x+y)
