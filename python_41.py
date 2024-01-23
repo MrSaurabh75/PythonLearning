0bb5f1da0c79fd67fdc4d26d1bbe78a38ab94f32
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-01-23 20:09:46 +0530

    Learned Short hand if-else

---
+++ python_41.py
@@ -0,0 +1,8 @@
+# Short hand if-else --> There is also a shorthand syntax for if-else statement that can be used when the condtion being tested is simple and the code blocks to be executed are short 
+a = 10
+b = 20
+print("a") if(a>b) else print("a=b") if(a==b) else print("b")
+
+c = 12
+d = 15
+print(f"{c}+{d}={c+d}") if c>d else print(f"{c}-{d}={c-d}") if(c==d) else print
