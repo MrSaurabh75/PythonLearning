61921e56fac7a7b3646a85a60e6ea0c6d8220c4c
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-02-18 09:06:14 +0530

    Practice Questions added

---
+++ Python_Practice_1st.py
@@ -0,0 +1,18 @@
+# Write a Program to demonstrate a year entered by the user is leap year or not
+# user = int(input("Enter a year : "))
+# if(user%4==0 and user%100!=0 or user%400==0):
+#     print(user," is a Leap Year")
+# else:
+#     print(user," isn't a Leap Year")
+
+# Write a program to demonstrate a number entered by user is prime or not
+# user = int(input("Enter a number : "))
+# for i in range(2,user+1):
+#     if (user%i)==0:
+#         break
+# if i==user:
+#     print(user," is a prime number")
+# else:
+#     print(user," is not a prime number")
+
+# Write a program to calculate the sum of even numbers from 1 t0 20
