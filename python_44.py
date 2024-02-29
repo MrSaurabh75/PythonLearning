2697e71c54202308d18f2331eab11defd25f76ee
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-02-29 18:46:19 +0530

    Python Module and User Defined module

---
+++ python_44.py
@@ -0,0 +1,31 @@
+# How to import works in python.
+
+import math
+# print(math.sqrt(20))
+# print(math.floor(23.445))
+print(dir(math))
+
+
+# from math import sqrt,pi #imports only perticular mentioned functions
+# print(sqrt(9))
+# print(pi)
+# print(2*pi)
+
+# import UserDefined
+# a = 12
+# b = 24
+# print(f"Addition of {a} + {b} is ",UserDefined.add(a,b))
+
+# from UserDefined import sub
+# a = 90
+# b = 20
+# print(f"Addition of {a} - {b} is ",sub(a,b))
+
+# from UserDefined import sub as s
+# a = 90
+# b = 20
+# print(f"Addition of {a} - {b} is ",s(a,b))
+
+
+
+
