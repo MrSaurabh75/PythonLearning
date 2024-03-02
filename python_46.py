ac06b9e130a64db8cc229b3736433ac70142e3f7
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-03-02 10:12:30 +0530

    os module

---
+++ python_46.py
@@ -0,0 +1,16 @@
+# os module in python : Helps a programmer to us the oprating system with some functions 
+import os
+# if (not os.path.exists("data")):
+#     os.mkdir("data")
+# for i in range(0,100):
+#     os.mkdir(f"Data/Day {i+1}")
+
+# Rename function
+# import os
+# for i in range(0,100):
+#     os.rename(f"Data/Day {i+1}",f"Data/Tute {i+1}")
+
+# folders = os.listdir("data")
+# for folder in folders:
+#     print(folder)
+#     print(os.listdir(f"Data/{folder}"))
