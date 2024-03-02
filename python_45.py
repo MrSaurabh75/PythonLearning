e723ce5509c41dba9a70f63a6ebf3bf6b2f285f8
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-03-02 07:27:47 +0530

    if __name__==main ideom

---
+++ python_45.py
@@ -0,0 +1,4 @@
+# if __name__=="__main__": in python
+# Ideom is a common pattern is used in python scripts to determine wheather thescript is being run directly or being imported from a module into another script 
+import welcome
+welcome.guest()
---
+++ welcome.py
@@ -0,0 +1,5 @@
+def guest():
+    print("Welcome Guest!")
+
+if __name__=="__main__":
+    guest()
