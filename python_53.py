6d8f22888d39cff9ccc093260dd44da7b3a4603d
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-03-03 07:58:54 +0530

    Map,Filter and Reduce

---
+++ python_53.py
@@ -0,0 +1,38 @@
+# Map,Filter and Reduce
+
+# Map
+# def cube(x):
+#     return x*x*x
+l = [1,2,3,45,5,67,4,6,8,9,3]
+# newl = list(map(cube,l))
+# print(newl)
+
+# Filter
+# def fil_fun(x):
+#     if x%2==0:
+#         return x
+# new2 = list(filter(fil_fun,l))
+# print(new2)
+
+# Reduce
+# from functools import reduce
+# def mysum(x,y):
+#     return x+y
+# sum = reduce(mysum,l)
+# print(sum)
+
+# Map function with lambda function
+# cube = lambda x:x*x*x
+# new3 = list(map(cube,l))
+# print(new3)
+
+# Filter function with lambda function
+# even = lambda x:x%2==0
+# new4 = list(filter(even,l))
+# print(new4)
+
+# Reduce function with lambda function
+# from functools import reduce
+# sum = lambda x,y:x+y
+# new5 = reduce(sum,l)
+# print(new5)
