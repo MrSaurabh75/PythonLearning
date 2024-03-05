6fbdf4a134d1076d6ea29053b78069c9ca5e3d45
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-03-05 06:41:47 +0530

    Introduction to OOPs

---
+++ python_56.py
@@ -0,0 +1,6 @@
+# Introduction to oops : In a python Object Oriented Programming is to use the classes and objects to represent the real world concept
+# Class
+# Object
+# Inheritance
+# Encapsulation
+# Polymorphism
