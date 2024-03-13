1084ceee2abd5b1003cf16d0d2638244984d41de
MrSaurabh75 <saurabhdc7575@gmail.com>
2024-03-13 08:39:50 +0530

    Hierarchical Inheritance

---
+++ python_81.py
@@ -0,0 +1,19 @@
+# Hybrid Inheritance
+class Base:
+    pass
+class Derived1(Base):
+    pass
+class Derived2(Base):
+    pass
+class Derived3(Derived1,Derived2):
+    pass
+
+# Hierarchical Inheritance
+class BaseClass:
+    pass
+class t1(BaseClass):
+    pass
+class t2(BaseClass):
+    pass
+class t3(BaseClass):
+    pass
